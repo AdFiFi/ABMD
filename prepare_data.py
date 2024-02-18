a5a65c153db488eda58d14f76ce21ecbbadb7bd4
AdFiFi <116166221+AdFiFi@users.noreply.github.com>
2024-02-18 20:54:11 +0800

    Delete prepare_data.py

--- prepare_data.py
+++
@@ -1,6 +0,0 @@
-from data.zuco2 import zuco_preprocess_TSR
-from data.mnred2 import eeg_preprocess_test
-
-if __name__ == '__main__':
-    # zuco_preprocess_TSR(path=r"D:\data\ZuCo2")
-    eeg_preprocess_test(path=r"/data/datasets/MNRED2")
